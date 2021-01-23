6425940e9f8c15431e60a39a2e7688d2254965bc
90306708 <90306708@hs-34719-macair.ad.edenpr.org>
2021-01-23 11:54:51 -0600

    Added robotconfiglowgear from 2020 code

---
+++ robotconfiglowgear.py
@@ -0,0 +1,28 @@
+{
+    "rightControllerTypes": ["WPI_TalonFX", "WPI_TalonFX"],
+    "leftControllerTypes": ["WPI_TalonFX", "WPI_TalonFX"],
+
+    "leftMotorPorts": [3, 4],
+    "rightMotorPorts": [1, 2],
+    "leftMotorsInverted": [False, False],
+    "rightMotorsInverted": [False, False],
+
+    # (in meters)
+    "wheelDiameter": 0.1524,
+
+    # Low Gear
+    "encoderEPR": 257.4628571,
+
+    "leftEncoderInverted": False,
+    "rightEncoderInverted": False,
+
+    "gyroType": "NavX",
+    "gyroPort": "SPI.Port.kMXP",
+}
+
+
+
+
+
+
+
