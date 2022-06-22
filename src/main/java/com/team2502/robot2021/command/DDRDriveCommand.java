4a8ee33ed8ecef38753c2c4811b939bf51398a19
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2022-06-22 13:26:09 -0500

    added konami code

--- src/main/java/com/team2502/robot2021/command/DDRDriveCommand.java
+++ src/main/java/com/team2502/robot2021/command/DDRDriveCommand.java
@@ -5,6 +5,8 @@ import com.team2502.robot2021.subsystem.DrivetrainSubsystem;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+import com.team2502.robot2021.util.KonamiHandler;
+import com.team2502.robot2021.util.KonamiHandler.BUTTONS;
 import com.team2502.robot2021.util.Trapezoidal;
 import com.team2502.robot2021.Constants;
 
@@ -14,12 +16,14 @@ public class DDRDriveCommand extends CommandBase {
     private final Joystick groovyJoystick;
     private Trapezoidal speedTrapezoidal;
     private Trapezoidal rotationTrapezoidal;
+    private KonamiHandler konamiHandler;
 
     public DDRDriveCommand(DrivetrainSubsystem drivetrain, Joystick groovyJoystick) {
         this.drivetrain = drivetrain;
         this.groovyJoystick = groovyJoystick;
         this.speedTrapezoidal = new Trapezoidal(2);
         this.rotationTrapezoidal = new Trapezoidal(2);
+        this.konamiHandler = new KonamiHandler();
 
         addRequirements(drivetrain);
     }
@@ -28,7 +32,27 @@ public class DDRDriveCommand extends CommandBase {
     public void execute() {
         double speed = 0;
         double rotation = 0;
-        final double topSpeed = .4;
+
+        double topSpeed = konamiHandler.isFinished() ? 1 : .4;
+
+        if (groovyJoystick.getRawButtonPressed(Constants.OI.DDR_UP)) {
+            konamiHandler.handle(BUTTONS.UP);
+        }
+        if (groovyJoystick.getRawButtonPressed(Constants.OI.DDR_DOWN)) {
+            konamiHandler.handle(BUTTONS.DOWN);
+        }
+        if (groovyJoystick.getRawButtonPressed(Constants.OI.DDR_LEFT)) {
+            konamiHandler.handle(BUTTONS.LEFT);
+        }
+        if (groovyJoystick.getRawButtonPressed(Constants.OI.DDR_RIGHT)) {
+            konamiHandler.handle(BUTTONS.RIGHT);
+        }
+        if (groovyJoystick.getRawButtonPressed(Constants.OI.DDR_FLYWHEEL)) {
+            konamiHandler.handle(BUTTONS.A);
+        }
+        if (groovyJoystick.getRawButtonPressed(Constants.OI.DDR_TURRET_RIGHT)) {
+            konamiHandler.handle(BUTTONS.B);
+        }
 
         if (groovyJoystick.getRawButton(Constants.OI.DDR_UP)) {
             speed = topSpeed;
@@ -42,9 +66,6 @@ public class DDRDriveCommand extends CommandBase {
         if (groovyJoystick.getRawButton(Constants.OI.DDR_RIGHT)) {
             rotation = topSpeed;
         }
-
-
-
         
         drivetrain.getDrive().arcadeDrive(speedTrapezoidal.calculate(speed), rotationTrapezoidal.calculate(rotation));
     }
